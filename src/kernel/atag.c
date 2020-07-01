76a2e2d72ccb069b39b68c35635b3a5a2a2ecb54
Patrick Wolfe <patrick.wolfe@tufts.edu>
2020-07-01 09:00:36 -0400

    Initialize memory

--- src/kernel/atag.c
+++ src/kernel/atag.c
@@ -1,10 +1,12 @@
 #include <kernel/atag.h>
 
-uint32_t get_mem_size(atag_t * t) {
-    // Iterate over atags
+uint32_t get_mem_size(atag_t * tag) {
+    // Iterate over atags to find memory size
     while (tag->tag != NONE) {
         if (tag->tag == MEM) {
             return tag->mem.size;
         }
-        tag = ((uint32_t *) tag) + tag->tag_size;
+        tag = tag + tag->tag_size;
+    }
+    return 0;
 }
--- src/kernel/mem.c
+++ src/kernel/mem.c
@@ -11,16 +11,36 @@ static uint32_t num_pages;
 DEFINE_LIST(page);
 IMPLEMENT_LIST(page);
 
-static page_t * all_pages_array;
+static page_t * page_array;
 page_list_t free_pages;
 
 void mem_init(atag_t * atags) {
-    uint32_t mem_size, page_array_length;
+    uint32_t mem_size, page_array_size, kernel_pages, i;
 
     // Calculate # of pages    
     mem_size = get_mem_size(atags);
     num_pages = mem_size / PAGE_SIZE;
 
-    // Allocate space for page metadata
-    page_array_length = 
+    // # of bytes needed for page metadata
+    page_array_size = num_pages * sizeof(page_t);
+
+    // Pointer to end of image / beginning of page array
+    page_array = (page_t *)&__end;
+    bzero(page_array, page_array_size);
+
+    // Number of pages used for kernel
+    kernel_pages = ((uint32_t)&__end) / PAGE_SIZE;
+
+    // Kernel pages flags
+    for (i = 0; i < kernel_pages; i++) {
+        page_array[i].vaddr_mapped = i * PAGE_SIZE;
+        page_array[i].flags.allocated = 1;
+        page_array[i].flags.kernel_page = 1;
+    }
+
+    // Free page flags 
+    for (; i < num_pages; i++) {
+        page_array[i].flags.allocated = 0;
+        append_page_list(&free_pages, &page_array[i]);
+    }
 }
